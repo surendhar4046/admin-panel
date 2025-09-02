9d42ad37fb9060937246defa893799340301bd79
surendhar4046 <surendharv4046@gmail.com>
2025-09-02 15:14:56 +0530

    update3

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
-import React from "react";
+import React, { useState } from "react";
 import Dashboard from "./Dashboard";
 
 function App() {
-  const [showDashboard, setShowDashboard] = React.useState(false);
+  const [showDashboard, setShowDashboard] = useState(false);
 
   return (
     <div style={{ textAlign: "center", marginTop: "50px" }}>
       {!showDashboard ? (
         <>
-          <h1>Welcome</h1>
-          <button onClick={() => setShowDashboard(true)}>
+          <h1>WELCOME</h1>
+          <button
+            style={{ marginTop: "20px", padding: "10px 20px" }}
+            onClick={() => setShowDashboard(true)}
+          >
             Go to Dashboard
           </button>
         </>
